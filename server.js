155e98bd6e1a0a98dc32f168b3a9bb00d4b2d6c9
tuhwe005 <96981681+tuhwe005@users.noreply.github.com>
2023-01-28 21:52:10 +0800

    Update server.js

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ app.use(
 /* keepalive  begin */
 function keepalive() {
   // 1.请求主页，保持唤醒
-  let app_url = "https://fish-far-production.glitch.me";
+  let app_url = "wild-ruby-vulture-wig.cyclic.app";
   exec("curl " + app_url, function (err, stdout, stderr) {
     if (err) {
       console.log("保活-请求主页-命令行执行错误：" + err);
